0a02b0196aa1efcf991b0e67c4a2fae84113a0af
Victor Carneiro Lima <victor.lima@lnls283-vm.abtlus.org.br>
2019-10-08 17:19:42 -0300

    Imagem FSK adicionada

--- TX_script.py
+++ TX_script.py
@@ -1,34 +1,36 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-baudRate=60
+baudRate=10
 
-Ts=16000
-F1=1000
-F2=4000
-t=np.arange(0,1/baudRate,1/Ts)
+Fs=16000
+F1=50
+F2=200
+t=np.arange(0,1/baudRate,1/Fs)
 
 wave1=np.sin(2*np.pi*F1*t)
 wave2=np.sin(2*np.pi*F2*t)
 
-square1=np.ones((1,1/baudRate))
-square2=-np.ones((1,1/baudRate))
-
-sine_output=np.array([])
-square_output=np.array([])
+square1=np.ones((1,int(Fs/baudRate)))
+square2=-np.ones((1,int(Fs/baudRate)))
+sine_output=np.array([0])
+square_output=np.array([0])
 
 bit_string=[0,0,1,0,1]
 
-for i in arange(len(bit_string)):
+x=np.arange(0,(len(bit_string)/baudRate)+1,1/Fs)
+x=x[0:len(bit_string)*len(t)]
+
+for i in range(len(bit_string)):
     if(bit_string[i]==1):
-        np.append(sine_output,wave1)
-        np.append(square_output,square1)
+        sine_output=np.append(sine_output,wave1)
+        square_output=np.append(square_output,square1)
     else:
-        np.append(sine_output,wave2)
-        np.append(square_output,square2)
+        sine_output=np.append(sine_output,wave2)
+        square_output=np.append(square_output,square2)
 
 fig, axs = plt.subplots(2)
-fig.suptitle('Vertically stacked subplots')
-axs[0].plot(t, sine_output)
-axs[1].plot(t, square_output)
+fig.suptitle('FSK - F1:50Hz F2=200Hz')
+axs[0].plot(x, sine_output[0:len(x)])
+axs[1].plot(x, square_output[0:len(x)])
 plt.show()
