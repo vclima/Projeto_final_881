95472efffb79858e7a522a9f2eede5d13474128a
Victor Lima <vlima@localhost.localdomain>
2019-10-09 22:40:20 -0300

    Implementada soluçao de senos e cossenos

--- Exemplos/exemplo_casado.py
+++ Exemplos/exemplo_casado.py
@@ -101,4 +101,28 @@ axs[0].plot(t,casado_1[0:len(t)])
 axs[0].plot(t_amostra,amostra_casado1[0:len(t_amostra)],'or')
 axs[1].plot(t,casado_2[0:len(t)])
 axs[1].plot(t_amostra,amostra_casado2[0:len(t_amostra)],'or')
+plt.show(block=False)
+
+#Outra implementação:
+#Filtro casado com erro de fase de 90°
+wave1_sin=np.sin(2*np.pi*F1*t_wave+np.pi/2)
+wave1_cos=np.cos(2*np.pi*F1*t_wave+np.pi/2)
+wave2_sin=np.sin(2*np.pi*F2*t_wave+np.pi/2)
+wave2_cos=np.cos(2*np.pi*F2*t_wave+np.pi/2)
+
+#Aplicação do filtro casado + média móvel
+casado_1=np.power(np.convolve(data,np.flip(wave1_sin)),2)+np.power(np.convolve(data,np.flip(wave1_cos)),2)
+casado_2=np.power(np.convolve(data,np.flip(wave2_sin)),2)+np.power(np.convolve(data,np.flip(wave2_cos)),2)
+
+amostra_casado1=casado_1[step::step]
+amostra_casado2=casado_2[step::step]
+t_amostra=np.arange(step/Fs,t[-1]+step/Fs,step/Fs)
+
+
+fig, axs = plt.subplots(2)
+fig.suptitle('Saída dos filtros casados + decomposição em seno e cosseno- Erro de 90°')
+axs[0].plot(t,casado_1[0:len(t)])
+axs[0].plot(t_amostra,amostra_casado1[0:len(t_amostra)],'or')
+axs[1].plot(t,casado_2[0:len(t)])
+axs[1].plot(t_amostra,amostra_casado2[0:len(t_amostra)],'or')
 plt.show()
